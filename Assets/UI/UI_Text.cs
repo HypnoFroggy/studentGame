164c293600efaf1e151555dbe86bdc776147425c
MasterOfUniverses <DrStephenVincentStrange@yandex.ru>
2024-05-28 12:41:18 +0300

    small UI fix

--- Assets/UI/UI_Text.cs
+++ Assets/UI/UI_Text.cs
@@ -11,11 +11,16 @@ public class UI_Text : MonoBehaviour
     public Text timebox;
     public Parameters PlayerParameters;
 
+    private const int ms_in_sec = 1000;
+    private const int ms_in_min = ms_in_sec*60;
+    private const int ms_in_hour = ms_in_min*60;
+    private const int ms_in_day = ms_in_hour*24;
     void Update()
     {
-        thirstbox.text = PlayerParameters.thirst.ToString();
-        hungerbox.text = PlayerParameters.hunger.ToString();
-        timebox.text = PlayerParameters.time.ToString();
-        happinessbox.text = PlayerParameters.happiness.ToString();
+        thirstbox.text = (PlayerParameters.thirst).ToString("N2");
+        hungerbox.text = (PlayerParameters.hunger).ToString("N2");
+        float time = PlayerParameters.time;
+        timebox.text = string.Format("Δενό {0} {1:D2}:{2:D2}:{3:D2}", (int)(time/ms_in_day)+1, (int)((time % ms_in_day)/ms_in_hour), (int)((time % ms_in_hour) / ms_in_min), (int)((time % ms_in_min)/ms_in_sec));
+        happinessbox.text = (PlayerParameters.happiness).ToString("N2");
     }
 }
